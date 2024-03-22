e4b6252bb9168e3f44f62e58e3771324141b3ff7
Hailo7ts <101282320+Hailo7ts@users.noreply.github.com>
2024-03-22 13:48:30 -0600

    added components

---
+++ mimi-cro.ator/src/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header(){
+    return(
+        <header id="header">
+			<div class="inner">
+
+					<a href="index.html" class="logo">
+						<span class="symbol"><img src="images/mimi's-corner-logo.png" alt="" /></span><span class="title">Mimi-cro.ator</span>
+					</a>
+
+                
+					<nav>
+						<ul>
+							<li><a href="#menu">Menu</a></li>
+						</ul>
+					</nav>
+
+			</div>
+		</header>
+    )
+}
---
+++ mimi-cro.ator/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+export default function Menu(){
+    return(
+        <nav id="menu">
+			<h2>Menu</h2>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="tops.html">Tops</a></li>
+				<li><a href="hats.html">Hats</a></li>
+				<li><a href="bags.html">Bags</a></li>
+				<li><a href="amigurumi.html">Amigurumi</a></li>
+				<li><a href="elements.html">Elements</a></li>
+			</ul>
+		</nav>
+    )
+}
